f4749799c2023023fea8bd537d69a81df2cf9522
Thomas Jones <Thomasajones@hotmail.co.uk>
2024-07-16 19:49:06 +0100

    ground movement commit

--- js/script.js
+++ js/script.js
@@ -4,9 +4,12 @@ const game_world_height = 30;
 
 // DATA ATTRIBUTES
 const game_world_size = document.querySelector("[data-game]");
+const ground_elements = document.querySelectorAll("[data-ground]");
 
 setGameWorldSize();
 window.addEventListener("resize", setGameWorldSize);
+
+groundRandomGenerator();
 
 // UPDATE LOOP: GAME ITEMS
 let latestTime;
@@ -18,7 +21,9 @@ function updateGameItems(time) {
     return;
   }
   const gameFrames = time - latestTime;
-  console.log(gameFrames);
+  updateGroundPosition(gameFrames, 1);
+
+  // console.log(gameFrames);
 
   latestTime = time;
   window.requestAnimationFrame(updateGameItems);
@@ -41,3 +46,52 @@ function setGameWorldSize() {
   game_world_size.style.width = `${game_world_width * worldScale}px`;
   game_world_size.style.height = `${game_world_height * worldScale}px`;
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// GROUND ELEMENT
+
+const frame = 0.05;
+
+function updateGroundPosition(gameFrames, speedFrames) {
+  ground_elements.forEach((ground) => {
+    updateProperty(ground, "--left", gameFrames * speedFrames * frame * -1);
+
+    // Fix ground bug: ground runs out. (Ground: Infinite Loop)
+    if (getProperty(ground, "--left") <= -300) {
+      updateProperty(ground, "--left", 600);
+    }
+  });
+}
+
+function groundRandomGenerator() {
+  setProperty(ground_elements[0], "--left", 0);
+  setProperty(ground_elements[1], "--left", 300);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// HELPER FUNCTIONS
+
+function getProperty(elem, prop) {
+  // Get CSS Variables to utilise in JavaScript
+  // If no CSS variable number: make it 0.
+  return parseFloat(getComputedStyle(elem).getPropertyValue(prop)) || 0;
+}
+
+function setProperty(elem, prop, value) {
+  elem.style.setProperty(prop, value);
+}
+
+// getting the current value: adding an increment to the getProperty() function, and then setting the value.
+function updateProperty(elem, prop, inc) {
+  setProperty(elem, prop, getProperty(elem, prop) + inc);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////
